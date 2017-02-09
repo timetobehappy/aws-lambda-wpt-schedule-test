e6e9273305c0dd69e5ec0a363f7bb930fc5e62ac
timetobehappy <jibiu@outlook.com>
2017-02-09 16:09:47 -0600

    Added support for environment variables

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
 'use strict';
 
-var request = require('request');
+let request = require('request');
 
+let url = process.env.url;
+let key = process.env.key;
+let label = process.env.label;
+let testLocation = process.env.testLocation;
+let browser = process.env.browser;
+let connectivity = process.env.connectivity;
 
+let wptApi = `http://www.webpagetest.org/runtest.php?url=${url}&k=${key}&label=${label}&location=${testLocation}:${browser}.${connectivity}&video=1`;
 
 exports.handler = (event, context, callback) => {
-    //console.log('Received event:', JSON.stringify(event, null, 2));
-    request('http://www.webpagetest.org/runtest.php?url=www.example.com&k=YOURWPTKEY&label=YOURLABELFOREASYSEARCH&location=LOCATION:Chrome&video=1', function (error, response, body) {
-    console.log("www.example.com");
-    console.log(response.statusCode);
-    callback(null,response.statusCode);
-})
 
+    //console.log(wptApi);
+
+    request(wptApi, (error, response, body) => {
+        if (!error && response.statusCode == 200) {
+            callback(null, `${url} test submitted!`);
+        } else {
+            callback(new Error('Test failed!'));
+        }
+    });
 
 
 };
